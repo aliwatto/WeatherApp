8238c1998d91fe10bfce98cb873c25a3c43eaf82
ali-wattoo <m.ali@forenax.com>
2022-09-21 19:59:43 +0500

    Weather app done

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { WeatherService } from '../app/services/weather.service';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  weather: any
+  weatherTemp: any;
+  windSpeed: any
+  cityName: string = 'Islamabad'
+  constructor(public httpClient: HttpClient) {
+    this.loadData(this.cityName)
+  }
+
+  loadData(cityName: any) {
+    this.httpClient.get(`${API_URL}/weather?q=${cityName}&mode=json&units=metric&appid=${API_KEY}`).subscribe((results: any) => {
+      console.log(results);
+      this.weather = results
+      console.log(this.weather);
+      this.weatherTemp = results['main'];
+      this.windSpeed = results['wind'];
+      // console.log(this.windSpeed);
+    })
+  }
+
+  onSubmit() {
+    this.loadData(this.cityName)
+    this.cityName = ''
+
+  }
+
+}
+
+
+
+  // constructor(private weatherService: WeatherService) {
+
+
+  // }
+  // ngOnInit(): void {
+
+  //   // this.weatherService.getWeatherData('landon')
+
+  // }
+
+
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+  // XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  // XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  // XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  // XRapidAPIKeyHeaderValue: '7641ecf87emshd586d75958dc71fp13a39fjsn2ba81147984e',
+
+  API_KEY: "20f394d057685c26d17b3e3d400d458c",
+  API_URL: "https://api.openweathermap.org/data/2.5/"
+
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
